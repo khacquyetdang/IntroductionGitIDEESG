dfaedf9b4017573960e742f449534d69fd20ec6d
Tdang <tdang>
2024-10-16 11:05:10 +0200

    ajouter les types

--- Hellowolrd.py
+++ Hellowolrd.py
@@ -5,3 +5,18 @@ prenom = "Marie"
 age = 19
 print("Je m'appelle", prenom)
 print("J'ai ", age, " ans ")
+
+# example pour les types de données
+
+# int les nombres entier
+# float les nombre décimaux
+# str chaines de caractères
+# bool booléans (True ou False"
+
+entier = 5
+decimal = 10.05
+texte = "Bonjour ESG"
+Vrai = True # booleen
+
+print(type(entier))
+print(type(Vrai))
