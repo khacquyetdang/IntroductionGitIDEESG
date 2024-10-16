54bf9cac0a20e0ed04aefc6ae2e09313ecb2638e
Tdang <tdang>
2024-10-16 11:22:55 +0200

    ajouter les opérations mathémathique

---
+++ Introduction.py
@@ -0,0 +1,62 @@
+print("Bonjour les futures Ingénieurs en MBA de ESG")
+
+# example de variable
+prenom = "Marie"
+age = 19
+print("Je m'appelle", prenom)
+print("J'ai ", age, " ans ")
+
+# example pour les types de données
+
+# int les nombres entier
+# float les nombre décimaux
+# str chaines de caractères
+# bool booléans (True ou False"
+
+entier = 5
+decimal = 10.05
+texte = "Bonjour ESG"
+Vrai = True # booleen
+
+print(type(entier))
+print(type(Vrai))
+
+# les conditions
+# if else
+
+isESGGoodScool = True
+
+if (isESGGoodScool):
+ print("ESG est une excellente école")
+else: 
+ print("C'est impossible")
+
+neigeEnEte = False
+if (neigeEnEte):
+ print("Il neige en ete")
+else: 
+ print("En été, il ne neige pas")
+
+
+# Les opérateurs
+# + - * / (addition soustraction mutiplication et division
+
+
+CP=6
+CE1=CP+1
+print("CP", CP)
+print("CE1", CE1)
+
+MS=CP-2
+print("MS", MS)
+
+BAC=18
+CM1=BAC/2
+print("BAC", BAC)
+print("CM1", CM1)
+
+
+Cinquieme=CP*2
+print("5 eme", Cinquieme)
+
+
